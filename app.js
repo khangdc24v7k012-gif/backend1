320305f85b28ae0b255464332e8b5c01b3342df0
NAK-203 <khangdc24v7k012@vlvh.ctu.edu.vn>
2026-04-13 21:54:30 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
+const ApiError = require("./app/api-error");
 const contactsRouter = require("./app/routes/contact.route");
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+// routes
 app.use("/api/contacts", contactsRouter);
+
+// test root
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book API" });
+});
+
+// 404 handler
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
 });
 
 module.exports = app;
